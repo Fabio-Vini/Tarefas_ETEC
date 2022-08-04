f3f8a52565dc4f8f0026fff0ad23505ae2fcb20c
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2022-08-03 22:18:33 -0300

    Update C#(exercicio 9).cs

--- Exercicios C#/C#(exercicio 9).cs
+++ Exercicios C#/C#(exercicio 9).cs
@@ -11,21 +11,21 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
         /*Escreva um	programa para	ler o ano de nascimento de	
-         uma	pessoa e escrever uma mensagem que diga	se ela	poder· ou 
-         n„o votar este ano (n„o	È	necess·rio	considerar	o	mÍs
+         uma	pessoa e escrever uma mensagem que diga	se ela	poder√° ou 
+         n√£o votar este ano (n√£o √© necess√°rio considerar o m√™s
          em que ela nasceu).*/
          
-            Console.WriteLine("Digite o ano de seu nascimento, confira se pode votar ou n„o: ");
+            Console.WriteLine("Digite o ano de seu nascimento, confira se pode votar ou n√£o: ");
 
             int ano = int.Parse(Console.ReadLine());
 
             if(ano <= 2004)
             {
-                Console.WriteLine("VocÍ pode votar!");
+                Console.WriteLine("Voc√™ pode votar!");
             }
             else
             {
-                Console.WriteLine("N„o tem idade suficiente!");
+                Console.WriteLine("N√£o tem idade suficiente!");
             }
             Console.ReadKey();
         }
